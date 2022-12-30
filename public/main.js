af027b28ec63ecde22676ba6eef67fb4f8f1fbb8
Alyssa Padayhag <alyssapadayhag@gmail.com>
2022-12-30 12:05:10 -0500

    redid images, added js logic, added color

---
+++ public/main.js
@@ -0,0 +1,8 @@
+function imgSlider(image) {
+    document.querySelector(".drink").src = image;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector(".circle");
+    circle.style.background = color;
+}
